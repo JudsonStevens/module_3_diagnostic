8458bc5239b37f6ed48e8a2215b0338bc185f4f4
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-11 09:30:36 -0600

    Building feature test, WIP

---
+++ spec/features/user_can_search_and_see_returned_locations_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'User' do
+  it 'should be able to search from the root page and see a list of results' do
+
+    visit('/')
+    fill_in(:q, with: 80203)
+    click_on("Locate")
+
+    expect(current_path).to eq('/search')
+
+    expect(page).to have_content("Name: UDR")
+    expect(page).to have_content("Address: 800 Acoma St, Denver CO 80204")
+    expect(page).to have_content("Fuel Types: ELEC")
+    expect(page).to have_content("Distance: 0.314")
+    expect(page).to have_content("Access Times: 24-Hour")
+
+    expect(page).to have_content("Name: PUBLIC STATIONS")
+    expect(page).to have_content("Address: 1370 Elati St, Denver CO 80204")
+    expect(page).to have_content("Fuel Types: ELEC")
+    expect(page).to have_content("Distance: 0.763")
+    expect(page).to have_content("Access Times: 24-Hour")
+  end
+end
