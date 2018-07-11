8b15f535185f3d2f318ab576d3e5d88e501e0311
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-11 11:00:09 -0600

    Refactoring feature test

--- spec/features/user_can_search_and_see_returned_locations_spec.rb
+++ spec/features/user_can_search_and_see_returned_locations_spec.rb
@@ -9,20 +9,25 @@ describe 'User' do
 
       expect(current_path).to eq('/search')
 
-      expect(page).to have_content('Name:', count: 10)
-      expect(page).to_not have_content('Name:', count: 11)
-      expect(page).to_not have_content('Name:', count: 9)
+      expect(page).to have_content('Station Name:', count: 10)
+      expect(page).to_not have_content('Station Name:', count: 11)
+      expect(page).to_not have_content('Station Name:', count: 9)
+      expect(page).to_not have_content('Fuel Types: BD')
+      expect(page).to_not have_content('Fuel Types: CNG')
+      expect(page).to_not have_content('Fuel Types: E85')
+      expect(page).to_not have_content('Fuel Types: HY')
+      expect(page).to_not have_content('Fuel Types: LNG')
 
-      expect(page).to have_content("Name: UDR")
+      expect(page).to have_content("Station Name: UDR")
       expect(page).to have_content("Address: 800 Acoma St, Denver CO 80204")
       expect(page).to have_content("Fuel Types: ELEC")
-      expect(page).to have_content("Distance: 0.314")
+      expect(page).to have_content("Distance: 0.314 miles")
       expect(page).to have_content("Access Times: 24 hours daily")
 
-      expect(page).to have_content("Name: PUBLIC STATIONS")
+      expect(page).to have_content("Station Name: PUBLIC STATIONS")
       expect(page).to have_content("Address: 1370 Elati St, Denver CO 80204")
       expect(page).to have_content("Fuel Types: ELEC")
-      expect(page).to have_content("Distance: 0.763")
+      expect(page).to have_content("Distance: 0.763 miles")
       expect(page).to have_content("Access Times: 24 hours daily")
     end
   end
